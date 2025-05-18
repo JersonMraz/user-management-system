79737e041f0416d9abc0e5025a52f91397951e68
Clifford <cliffordalferez3@gmail.com>
2025-05-18 16:18:00 +0800

    Updating Environments

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 export const environment = {
     production: false,
-    apiUrl: ''
+    apiUrl: 'https://alferez-dbhelper.onrender.com'
   };
   
   
